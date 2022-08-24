f43610a20e338b1d9ec48bbf6705ef26092f8eb1
EliteAsian <lavasnakegaming@gmail.com>
2022-08-24 13:10:49 -0400

    String unescaping

--- src/main/java/com/scopelang/Utils.java
+++ src/main/java/com/scopelang/Utils.java
@@ -1,11 +1,15 @@
 package com.scopelang;
 
+import org.apache.commons.text.StringEscapeUtils;
+
 public final class Utils {
 	private Utils() {
 
 	}
 
 	public static String processLiteral(String str) {
-		return str.substring(1, str.length() - 1);
+		String out = str.substring(1, str.length() - 1);
+		out = StringEscapeUtils.unescapeJson(out);
+		return out;
 	}
 }
